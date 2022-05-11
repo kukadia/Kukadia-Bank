4853390ff081b1c87e4438f97b0701742459b81d
SR Kukadia <87287434+kukadia@users.noreply.github.com>
2022-05-10 22:18:47 -0700

    Initial Commit
    Initial Commit with basic banking functions

---
+++ dal.js
@@ -0,0 +1,83 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+let db    = null;
+
+//connect to mongo
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err,client){
+    console.log('Connected successfully to the server!');
+
+    //database name
+    const dbName = 'myproject';
+    db = client.db(dbName);
+});
+    //Create new user
+    function create(name,email,password){
+        return new Promise((resolve,reject) => {
+            const collection = db.collection('users');
+            const doc = {name, email,password,balance:0};
+                //insert into customer table
+            collection.insertOne(doc,{w:1}, function(err, result){
+                 err ? reject(err) : resolve(doc);
+             });
+        });
+    }
+
+
+// find user account
+function find(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .find({email: email})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+// find user account
+function findOne(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .findOne({email: email})
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));    
+    })
+}
+
+// update - deposit/withdraw amount
+function update(email, amount){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')            
+            .findOneAndUpdate(
+                {email: email},
+                { $inc: { balance: amount}},
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );            
+
+
+    });    
+}
+
+    //read from database all users
+    function all(){
+        return new Promise((resolve, reject) => {
+            const customers = db
+            .collection('users')
+            .find({})
+            .toArray(function(err,docs){
+                err ? reject(err) : resolve(docs);
+
+                //clean up
+                //client.close();
+            });
+            
+        });
+    }
+    
+    module.exports = {create, findOne, find, update, all};
---
+++ index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app     = express();
+var cors    = require('cors');
+var dal     = require('./dal.js');
+
+// used to serve satic files from public directory
+app.use(express.static('public'));
+app.use(cors());
+
+//create user account
+app.get('/account/create/:name/:email/:password', function(req,res){
+    dal.create(req.params.name,req.params.email,req.params.password)
+    .then((user) => {
+        console.log(user);
+        res.send(user);
+    }); 
+});
+
+//login user
+app.get('/account/login/:email/:password', function(req,res){
+    res.send({
+        email: req.params.email,
+        password: req.params.password,
+    });
+});
+
+//deposit
+app.get('/deposit/:email/:depositAmt', function(req,res){
+    dal.update(req.params.email,req.params.depositAmt)
+    .then((user) => {
+        console.log(user);
+        res.send(user);
+    }); 
+});
+
+//withdraw
+app.get('/withdraw/:email/:withdrawAmt', function(req,res){
+    res.send({
+        email: req.params.email,
+        withdrawAmt: Number(req.params.withdrawAmt),
+    });
+});
+
+//all accounts
+app.get('/account/all', function(req,res){
+    dal.all().then((docs)=>{
+        console.log(docs);
+        res.send(docs);
+    }) 
+});
+
+var port =3000;
+app.listen(port);
+console.log('Running on port:' + port);
